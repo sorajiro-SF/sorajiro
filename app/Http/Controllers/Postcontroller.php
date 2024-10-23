cd09bd57f7f9747caada81f259a6925239a1baf6
sorajiro-SF <tengtiantianya@gmail.com>
2024-10-23 22:03:49 +0000

    {07-2-3}

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -10,6 +10,6 @@ use Illuminate\Http\Request;
 class Postcontroller extends Controller
 {
     public function index(Post $post)
-    {return view('posts.index') ->with(['posts'=>$post->get()]);
+    {return view('posts.index') ->with(['posts'=>$post->getByLimit()]);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; //追記
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap();    //追記
+        // Paginator::useBootstrapFive();    公式ドキュメント
+       //または Paginator::useBootstrapFour();    公式ドキュメント
     }
 }
