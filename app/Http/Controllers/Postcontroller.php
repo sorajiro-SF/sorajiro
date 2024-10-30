b3a6b7e6d66fc02ec754b47a4684d7af43dc650d
sorajiro-SF <tengtiantianya@gmail.com>
2024-10-30 11:59:36 +0000

    third commit

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -11,6 +11,12 @@ use Illuminate\Http\Request;
 class Postcontroller extends Controller
 {
     public function index(Post $post)
-    {return view('posts.index') ->with(['posts'=>$post->getPagenateByLimit(3)]);
+    {
+        return view('posts.index') ->with(['posts'=>$post->getPagenateByLimit(5)]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post'=> $post]);
     }
 }
