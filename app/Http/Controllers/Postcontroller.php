27e180206941803fa14580f3f567771caf5baced
sorajiro-SF <tengtiantianya@gmail.com>
2024-11-05 21:53:48 +0000

    eight

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -6,8 +6,6 @@ use App\Models\Post;
 
 use App\Http\Requests\PostRequest;
 
-
-
 class Postcontroller extends Controller
 {
     public function index(Post $post)
@@ -29,15 +27,23 @@ class Postcontroller extends Controller
         $post->fill($input)->save();
         return redirect('/post/' .$post->id);
     }
+
     public function edit(Post $post)
     {
         return view('posts.edit')->with(['post'=>$post]);
     }
+
     public function update(PostRequest $request, Post $post)
     {
-        $input_post=$request['post'];
+        $input_post = $request['post'];
         $post->fill($input_post)->save();
 
-    return redirect('/posts/' . $post->id);
+        return redirect('/posts/' . $post->id);
+    }
+
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
